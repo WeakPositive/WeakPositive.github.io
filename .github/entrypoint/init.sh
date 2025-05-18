ceb036a797dedeb01f1e5a55514f3c857e95fec1
eq19 <eq19@users.noreply.github.com>
2025-05-18 09:55:15 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -4,6 +4,29 @@
 
 hr='------------------------------------------------------------------------------------'
 
+set_config() {
+  echo -e "\n$hr\nCONFIG\n$hr"
+  cat /home/runner/work/_actions/eq19/eq19/v2/.github/templates/jekyll_config.yml > $RUNNER_TEMP/_config.yml
+  export PATH=/home/runner/work/_actions/eq19/eq19/v2/.github/entrypoint:$PATH && source artifact.sh
+
+  cat $RUNNER_TEMP/orgs.json > $1/user_data/ft_client/test_client/results/orgs.json
+  gh variable set JEKYLL_CONFIG --body "$(cat $RUNNER_TEMP/_config.yml)"
+
+  PARAMS_JSON=$(curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
+    "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/PARAMS_JSON" | jq -r '.value')
+  echo "${PARAMS_JSON}" | jq '.' > $1/user_data/strategies/fibbo.json
+
+  if jq empty < $1/user_data/strategies/fibbo.json; then
+    echo -e "\n$hr\nPARAMETERS\n$hr"
+    cat $1/user_data/strategies/fibbo.json
+    gh variable set PARAMS_JSON --repo ${TARGET_REPOSITORY} --body "${PARAMS_JSON}"
+  else
+    echo "Invalid JSON"
+  fi
+
+  echo -e "\n$hr\nENVIRONTMENT\n$hr" && printenv | sort
+}
+
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
@@ -37,53 +60,14 @@ else
   echo 'LATEST_COMMIT='$LATEST_COMMIT >> ${GITHUB_ENV}
 fi
 
-if [[ -z ${PASS} ]] || [[ "${PASS}" == "true" ]]; then
-
-  echo 'TARGET_REPO='${TARGET_REPO} >> ${GITHUB_ENV}
-  echo 'REMOTE_REPO='${REMOTE_REPO} >> ${GITHUB_ENV}
-
-  if [[ -f /home/runner/_site/_config.yml ]]; then
-    cat /home/runner/_site/_config.yml
-    FOLDER=$(yq '.span' /home/runner/_site/_config.yml)
-    export FOLDER=$(eval echo $FOLDER)
-  elif [[ -f /home/runner/_site/.env ]]; then
-    set -a && . /home/runner/_site/.env && set +a
-  fi
-fi
-
 if [[ "${JOBS_ID}" == "1" ]]; then
 
-  if diff -qr ${GITHUB_WORKSPACE}/.github /home/runner/work/_actions/eq19/eq19/v2/.github >/dev/null; then
-    echo -e "\n$hr\nCONFIG\n$hr"
-    cat /home/runner/work/_actions/eq19/eq19/v2/.github/templates/jekyll_config.yml > $RUNNER_TEMP/_config.yml
-    export PATH=/home/runner/work/_actions/eq19/eq19/v2/.github/entrypoint:$PATH && source artifact.sh
-
-    cat $RUNNER_TEMP/orgs.json > $1/user_data/ft_client/test_client/results/orgs.json
-    gh variable set JEKYLL_CONFIG --body "$(cat $RUNNER_TEMP/_config.yml)"
-
-    PARAMS_JSON=$(curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
-      "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/PARAMS_JSON" | jq -r '.value')
-    echo "${PARAMS_JSON}" | jq '.' > $1/user_data/strategies/fibbo.json
-
-    if jq empty < $1/user_data/strategies/fibbo.json; then
-      echo -e "\n$hr\nPARAMETERS\n$hr"
-      cat $1/user_data/strategies/fibbo.json
-      gh variable set PARAMS_JSON --repo ${TARGET_REPOSITORY} --body "${PARAMS_JSON}"
-    else
-      echo "Invalid JSON"
-    fi
-    
-    echo -e "\n$hr\nENVIRONTMENT\n$hr"
-    printenv | sort
-  fi
-
-  echo -e "\n$hr\nWORKSPACE\n$hr"
-  cd ${GITHUB_WORKSPACE} && rm -rf .github
-  cp -r /home/runner/work/_actions/eq19/eq19/v2/.github .
-  chown -R "$(whoami)" .github
+  BASE_FOLDER="/home/runner/work/_actions/eq19/eq19/v2/.github"
+  if diff -qr ${GITHUB_WORKSPACE}/.github ${BASE_FOLDER} > /dev/null; then set_config $1; fi
+  cd ${GITHUB_WORKSPACE} && rm -rf .github && cp -r ${BASE_FOLDER} . && chown -R "$(whoami)" .github
 
   git remote set-url origin ${REMOTE_REPO}        
-  git add . && git commit -m "update workflows" --quiet && git push --quiet
+  CLEAN_STATUS=$(git add . && git commit -m "update workflows" --quiet && git push --quiet)
 
   if [[ $? -eq 0 ]]; then
 
@@ -98,8 +82,20 @@ if [[ "${JOBS_ID}" == "1" ]]; then
 
   else
 
+    if [[ ! -f $RUNNER_TEMP/_config.yml ]]; then set_config $1; fi
+    if [[ "$(yq '.repository' $RUNNER_TEMP/_config.yml)" != "$TARGET_REPOSITORY" ]]; then
+      echo "$(yq '.repository' $RUNNER_TEMP/_config.yml) != $TARGET_REPOSITORY"
+      curl -s -X POST \
+        -H "Authorization: token $GH_TOKEN" \
+        -H "Accept: application/vnd.github.v3+json" \
+        "https://api.github.com/repos/${GITHUB_REPOSITORY}/dispatches" \
+        -d '{"event_type": "retry_workflow", "client_payload": {"original_run_id": "${GITHUB_RUN_ID}"}}'
+      exit 1
+    fi
+
     cd $1 && javac -d user_data/ft_client/test_client javaCode/Main.java
-    cd $GITHUB_WORKSPACE && rm -rf user_data && mv -f $1/user_data . && ls -al .
+    cd $GITHUB_WORKSPACE && rm -rf user_data && mv -f $1/user_data .
+    echo -e "\n$hr\nWORKSPACE\n$hr" && ls -al .
 
     # Fetch SHA, encode new content, and update in one step
     gh api --method PUT /repos/${TARGET_REPOSITORY}/contents/.github/workflows/main.yml \
@@ -110,14 +106,12 @@ if [[ "${JOBS_ID}" == "1" ]]; then
 
 elif [[ "${JOBS_ID}" == "2" ]]; then
 
-  ls -alR $GITHUB_WORKSPACE
+  echo -e "\n$hr\nENVIRONTMENT\n$hr" && printenv | sort
+  echo -e "\n$hr\nWORKSPACE\n$hr" && ls -alR $GITHUB_WORKSPACE
 
   echo -e "\n$hr\nGH BRANCHES\n$hr"
   cd $RUNNER_TEMP && mkdir my-project && cd my-project && git init -q
   git remote add source "$REMOTE_REPO" && git remote add origin "$TARGET_REPO"
-
-  # Get fresh branch lists
-  git fetch --all
 
   # Get list of existing target branches
   existing_target_branches=$(git ls-remote --heads origin | awk -F'/' '{print $3}')
@@ -140,8 +134,9 @@ elif [[ "${JOBS_ID}" == "2" ]]; then
     else
       # Existing branch case
       if [[ "$local_branch" == "gh-pages" ]]; then
+        git fetch origin gh-pages
         # Check if 'docs/' exists in remote
-        if ! git ls-tree --name-only "origin/gh-pages" | grep -q "^docs/"; then
+        if ! git ls-tree --name-only "origin/gh-pages" | grep -q "^docs"; then
           echo "No docs/ found - recreating gh-pages"
           # Ensure local branch exists
           if ! git show-ref --verify --quiet "refs/heads/gh-pages"; then
@@ -158,11 +153,26 @@ elif [[ "${JOBS_ID}" == "2" ]]; then
   
 elif [[ "${JOBS_ID}" == "3" ]]; then
 
-  cd /home/runner/_site && rm -rf README.md docs && gist.sh ${BASE} $(pwd)
+  find -not -path "./.git/*" -not -name ".git" -delete
+  shopt -s dotglob && cp -R /mnt/disks/deeplearning/tmp/_site/* .
 
+  # Get the config value and save to file.json
+  curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
+    "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/ORGS_JSON" \
+    | jq -r '.value' > _data/orgs.json
+  curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
+    "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/JEKYLL_CONFIG" \
+    | jq -r '.value' > _config.yml
+
+  echo -e "\n$hr\nCONFIG\n$hr" && cat _config.yml
+  echo -e "\n$hr\nENVIRONTMENT\n$hr" && printenv | sort
+
+  gist.sh ${BASE} $(pwd)
   if [[ "${WIKI}" != "${BASE}" ]]; then
-    find . -type d -name "${FOLDER}" -prune -exec sh -c 'gist.sh ${WIKI} "$1"' sh {} \;
+    find . -type d -name "$(yq '.span' _config.yml)" -prune -exec sh -c 'gist.sh ${WIKI} "$1"' sh {} \;
   fi
+
+  echo -e "\n$hr\nWORKSPACE\n$hr" && ls -alR .
 
 else
 
@@ -175,14 +185,9 @@ else
   rm -rf ${RUNNER_TEMP//\\//}/gh-source/.git
   shopt -s dotglob && mv -f ${RUNNER_TEMP//\\//}/gh-source/* .
 
-  # Get the variable value and save to file.json
-  curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
-    "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/JEKYLL_CONFIG" \
-    | jq -r '.value' > _config.yml
-
   echo -e "\n$hr\nCONFIG\n$hr" && cat _config.yml
   echo -e "\n$hr\nENVIRONTMENT\n$hr" && printenv | sort
-  echo -e "\n$hr\nWORKSPACE\n$hr" && ls -lR .
+  echo -e "\n$hr\nWORKSPACE\n$hr" && ls -alR .
 
 fi
 
